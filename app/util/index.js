6cdfbf69aa4797af52a52540f9fd80d121477f6b
Alexandre Magno <amagnomonteiro@gmail.com>
2018-01-29 00:19:51 -0200

    remove only words regex

--- app/util/index.js
+++ app/util/index.js
@@ -1,5 +1,5 @@
 const buildSearchRegex = (str = '') => {
-  const strArray = Array.from(str.replace(/\W/ig, ''));
+  const strArray = Array.from(str);
   const regexArray = strArray.map(word => {
       if (word === 'a') return '[à-úÀ-ÚaA]';
       if (word === 'e') return '[à-úÀ-ÚeE]';
